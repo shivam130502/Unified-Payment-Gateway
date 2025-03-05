d6bfa35f7595b38333dd4fae85bd4c83a4bee1ae
shivam130502 <shivamdhir90@gmail.com>
2025-03-04 20:46:16 -0800

    currencyRateRepository updated to get findAll

--- src/main/java/com/shivam/pagos/service/PaymentTransactionService.java
+++ src/main/java/com/shivam/pagos/service/PaymentTransactionService.java
@@ -36,6 +36,7 @@ public class PaymentTransactionService {
 
 	public Object getAllTransactions() {
 		// TODO Auto-generated method stub
+        currencyRateRepository.findAll();
 		return null;
 	}
 
